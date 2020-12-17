0176c74ded85fafc882de5b128b9b82e2c3bad98
Aldo Pereira <aldopereira.gyn@gmail.com>
2020-12-17 20:12:06 -0300

    primeiro commit- configuração do projeto

---
+++ WebContent/cadastro-cliente.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Cadastro de Clientes</title>
+</head>
+<body>
+<form action="alunos" method="post">
+		CNPJ <input type="text" name="input-cnpj" size="30"/>
+		Nome <input type="text" name="input-nome" size="70"/>
+		<p>Nome Fantasia <input type="text" name="input-nomeFantasia" size="70"/></p>
+		
+		<p>CEP <input type="text" name="input-cep" size="30"/>
+		Logradouro <input type="text" name="input-logradouro" size="70"/></p>
+		
+		<p>Numero <input type="text" name="input-numero" size="30"/>
+		Complemento <input type="text" name="input-complemento" size="70"/></p>
+		
+		<p>UF <select id="cbx-uf" name="cbx-uf" style="width:60px">
+		</select>
+		
+		Municipio <select id="cbx-municipio" name="cbx-municipio" style="width:300px">
+		</select>
+		
+		</p>
+		
+		
+		
+		<p><input type="submit" name="btn-salva" value="salvar"/></p>
+	
+	</form>
+</body>
+</html>
---
+++ src/cobranca/entidade/Cliente.java
@@ -0,0 +1,93 @@
+package cobranca.entidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String cnpj;
+
+	private String nome;
+	private String nomeFantasia;
+	private String cep;
+	private String logradouro;
+	private int numero;
+	private String complemento;
+
+	@ManyToOne
+	private Municipio municipio;
+
+	public Cliente() {
+
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Municipio getMunicipio() {
+		return municipio;
+	}
+
+	public void setMunicipio(Municipio municipio) {
+		this.municipio = municipio;
+	}
+}
